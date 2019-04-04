284680f43dd7996e44d9148546f3e714dccb135f
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-04 13:26:11 +0700

    Improve concurrency

--- doujin.php
+++ doujin.php
@@ -8,13 +8,13 @@ $i = 0;
 unset($m[1][0], $m[1][1]);
 pcntl_signal(SIGCHLD, SIG_IGN);
 foreach ($m[1] as $k => &$v) {
-	if (!pcntl_fork()) {
+	if (!pcntl_fork())) {
 		download("http://servertoenof63yc.onion/freestuff/doujinshi/{$v}");	
 		exit;
 	}
 	$i++;
 
-	if (($i % 10) == 0) {
+	if (($i % 16) == 0) {
 		$status = null;
 		print "Waiting...\n";
 		pcntl_wait($status);
